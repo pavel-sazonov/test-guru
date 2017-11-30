0836d1012411cec197580ef28a1e05bc95d068d6
pavel-sazonov <wismyl@ya.ru>
2017-11-30 15:41:27 +0300

    Create model Question and table questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130112032) do
+ActiveRecord::Schema.define(version: 20171130123612) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body"
