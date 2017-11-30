875d57260011c0add7e488c74976aed51b0aa5ce
pavel-sazonov <wismyl@ya.ru>
2017-11-30 16:03:05 +0300

    Create model Ansewrs and table answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130123612) do
+ActiveRecord::Schema.define(version: 20171130125155) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body"
+    t.boolean "correct"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "body"
