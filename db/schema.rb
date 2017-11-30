9ed562065b333be8ec8e8a6a5b28b163f942e437
pavel-sazonov <wismyl@ya.ru>
2017-11-30 21:02:55 +0300

    Fix test level default=0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130165143) do
+ActiveRecord::Schema.define(version: 20171130175812) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20171130165143) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 0, null: false
+    t.integer "level", default: 0
     t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
