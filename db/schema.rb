1ba0cf6ed04f142f6cb51931976e7caa57d7b147
pavel-sazonov <wismyl@ya.ru>
2017-11-30 21:07:41 +0300

    Fix answer correct default

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130175812) do
+ActiveRecord::Schema.define(version: 20171130180408) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
-    t.boolean "correct", default: false, null: false
+    t.boolean "correct", default: false
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
