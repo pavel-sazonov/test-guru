84714597ef406bdebe15d0786628aac287ae8327
pavel-sazonov <wismyl@ya.ru>
2017-11-30 18:53:27 +0300

    Add test title level category id null constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130153153) do
+ActiveRecord::Schema.define(version: 20171130154733) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 20171130153153) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level"
-    t.integer "category_id"
+    t.string "title", null: false
+    t.integer "level", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
