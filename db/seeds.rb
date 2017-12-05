961d1d333008d3106f5e195963b20380ff53fb78
pavel-sazonov <wismyl@ya.ru>
2017-12-05 17:25:02 +0300

    Set values for Question and Answer

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,7 @@
 
 category = Category.create([
   { title: 'frontend'},
-  { title: 'backend'},
-  { title: 'mobile'}
+  { title: 'backend'}
   ])
 
 test = Test.create([
@@ -18,7 +17,32 @@ test = Test.create([
   { title: 'Javascript', level: 1, category_id: 1 },
   { title: 'Ruby', level: 2, category_id: 2 },
   { title: 'Rails', level: 3, category_id: 2 },
-  { title: 'Swift', level: 2, category_id: 3 },
   ])
+
+question = Question.create([
+  { title: 'Which tag is used to refer to a paragraph?', test_id: 1 },
+  { title: 'Which tag is used to set background color?', test_id: 2 },
+  { title: 'Show syntax to define a new function?', test_id: 3 },
+  { title: 'What method does use to show amount of elements in array?', test_id: 4 },
+  { title: 'How to start a local server?', test_id: 5 },
+  ])
+
+answer = Answer.create([
+  { body: '<p>', correct: true, question_id: 1 },
+  { body: '<br>', correct: false, question_id: 1 },
+  { body: 'background-color', correct: true, question_id: 2},
+  { body: 'color', correct: false, question_id: 2},
+  { body: 'function name(parameters) {};', correct: true, question_id: 3},
+  { body: 'func name(parameters) {};', correct: false, question_id: 3},
+  { body: 'count', correct: true, question_id: 4},
+  { body: 'size', correct: true, question_id: 4},
+  { body: 'elements', correct: false, question_id: 4},
+  { body: 'rails s', correct: true, question_id: 5},
+  { body: 'rails server', correct: true, question_id: 5},
+  { body: 'rails c', correct: false, question_id: 5},
+  ])
+
+
+
 
 
