8d4927e52539e39694ff87c2292c3a9eced13fba
pavel-sazonov <wismyl@ya.ru>
2017-12-06 11:55:45 +0300

    DESC for allTestTitleByCategory

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   def self.allTestTitleByCategory(categoryTitle)
-    return Test.where('category_id = 1').order('title').pluck(:title) if categoryTitle == 'frontend'
-    Test.where('category_id = 2').order('title').pluck(:title)
+    # DESC - по убыванию? (10, 9, 8 ...)
+    return Test.where('category_id = 1').order('title DESC').pluck(:title) if categoryTitle == 'frontend'
+    Test.where('category_id = 2').order('title DESC').pluck(:title)
   end
 end
