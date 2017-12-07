8ec748a329ba12203241e2baff8f8b3af0566805
pavel-sazonov <wismyl@ya.ru>
2017-12-07 15:10:26 +0300

    Fix all_test_title_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,15 +3,8 @@ class Test < ApplicationRecord
   def self.all_test_title_by_category(category_title)
 
     Test.joins("JOIN categories ON categories.id = tests.category_id")
-        .where("categories.title = ?", category_title).
-        pluck(:title)
-        .sort
-        .reverse
-
-    # Test.joins("JOIN categories ON categories.id = tests.category_id")
-    #     .where("categories.title = ?", category_title)
-    #     .map { |t| t.title }
-
-    # Test.joins("JOIN categories ON categories.id = tests.category_id").where("categories.title = 'frontend'").pluck(:title).sort.reverse
+        .where("categories.title = ?", category_title)
+        .order('tests.title DESC')
+        .pluck('tests.title')
   end
 end
