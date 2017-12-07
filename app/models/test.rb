8586c6962483bbbb7ba89ab860c5a51d17c0f738
pavel-sazonov <wismyl@ya.ru>
2017-12-07 14:15:03 +0300

    Add variant to all_test_title_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,17 @@
 class Test < ApplicationRecord
 
   def self.all_test_title_by_category(category_title)
+
     Test.joins("JOIN categories ON categories.id = tests.category_id")
-        .where("categories.title = ?", category_title)
-        .order('title DESC').map { |t| t.title }
+        .where("categories.title = ?", category_title).
+        pluck(:title)
+        .sort
+        .reverse
+
+    # Test.joins("JOIN categories ON categories.id = tests.category_id")
+    #     .where("categories.title = ?", category_title)
+    #     .map { |t| t.title }
+
+    # Test.joins("JOIN categories ON categories.id = tests.category_id").where("categories.title = 'frontend'").pluck(:title).sort.reverse
   end
 end
