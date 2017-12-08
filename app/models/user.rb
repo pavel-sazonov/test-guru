31da4c20cb5fbcb5d20a96b146c7324618265056
pavel-sazonov <wismyl@ya.ru>
2017-12-08 13:17:15 +0300

    Fix all_tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 
   def all_tests_by_level(test_level)
-    Test.where(level: test_level)
+    # users_tests - таблица с двумя полями: user_id, test_id
+    Test.joins('JOIN users_tests ON users_tests.test_id = tests.id')
+        .where('tests.level = ?', test_level)
+        .pluck('tests.title')
   end
 end
