5ce74808f4ef143bb2c839c378436ea66034dde6
pavel-sazonov <wismyl@ya.ru>
2017-12-12 18:24:24 +0300

    Define relationship between Category and Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,7 @@
 class Test < ApplicationRecord
 
+  belongs_to :category
+
   def self.all_test_title_by_category(category_title)
 
     Test.joins("JOIN categories ON categories.id = tests.category_id")
