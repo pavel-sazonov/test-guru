c06ea8a15023f47b61f23e60ef011bc33a94379f
pavel-sazonov <wismyl@ya.ru>
2017-12-20 15:03:07 +0300

    Change User method tests_by_level with association + scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -14,6 +14,8 @@ class Test < ApplicationRecord
   scope :middle, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
 
+  scope :by_level, -> (level) { where(level: level) }
+
 
   def self.all_test_title_by_category(category_title)
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,11 @@ class User < ApplicationRecord
 
   has_many :authored_tests, class_name: 'Test'
 
-  def tests_by_level(test_level)
-    Test.joins('JOIN users_tests ON users_tests.test_id = tests.id')
-        .where('tests.level = ? AND users_tests.user_id = ?', test_level, self.id)
+  # def tests_by_level(level)
+  #   Test.joins('JOIN users_tests ON users_tests.test_id = tests.id')
+  #       .where('tests.level = ? AND users_tests.user_id = ?', level, self.id)
+  # end
+  def tests_by_level(level)
+    self.tests.by_level(level)
   end
 end
