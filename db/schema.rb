c5cb2edb61f4cb2e2561419f9ab20fc81537b526
pavel-sazonov <wismyl@ya.ru>
2017-12-22 20:46:26 +0300

    Add unique index to tests title and level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221143643) do
+ActiveRecord::Schema.define(version: 20171222174258) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20171221143643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
