4b8fed7428f758e4181a27f7de452cd2ce9ff0c1
pavel-sazonov <wismyl@ya.ru>
2018-01-29 15:48:31 +0300

    add user id into session after auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ class ApplicationController < ActionController::Base
     cookies[:email] = current_user.email
   end
 
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
