cddfbb33b81501eb0117731e490c9de19d8b21c2
pavel-sazonov <wismyl@ya.ru>
2018-01-29 09:41:37 +0300

    remove manual auth and make it via has_secure_password method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-
-  include Auth
-
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :user_id
