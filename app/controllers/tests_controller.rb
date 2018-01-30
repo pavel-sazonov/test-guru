d1e221e1df14238fa4a306c10e9095a445b4bde4
pavel-sazonov <wismyl@ya.ru>
2018-01-30 14:04:45 +0300

    use current_user in tests#start

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -44,7 +44,7 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
+    current_user.tests.push(@test)
     redirect_to @user.test_passage(@test)
   end
 
