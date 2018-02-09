b30bb178f4e0cbb627cfe5fcd57419861a8b1b01
pavel-sazonov <wismyl@ya.ru>
2018-02-10 02:45:19 +0300

    fix flash notice in SessionsController#create

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def create
     super
-    flash[:notice] = "#{t('.welcome')}, #{current_user.full_name}"
+    flash[:notice] = t('.welcome', full_name: current_user.full_name)
   end
 end
