31c0046249c5838bb8e20ec41b2b26c4ae29ea63
pavel-sazonov <wismyl@ya.ru>
2018-02-17 18:41:49 +0300

    change migrations for heroku

--- db/migrate/20171215154150_remove_author_id_from_test.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAuthorIdFromTest < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :tests, :author_id, :integer
-  end
-end
--- db/migrate/20171215154252_remove_author_id_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAuthorIdFromUser < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :author_id, :integer
-  end
-end
