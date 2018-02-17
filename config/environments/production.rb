1614a6260c44b9240639074377cb729ed9c3140b
pavel-sazonov <wismyl@ya.ru>
2018-02-17 23:11:38 +0300

    fix ActionMailer setting for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'shrouded-meadow-23547.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
+    domain:               "shrouded-meadow-23547.herokuapp.com",
+    authentication:       "plain",
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true
   }
