dd279c93032956b35ffac1f9149630cd34b19916
pavel-sazonov <wismyl@ya.ru>
2018-02-17 18:36:31 +0300

    change migrations

--- db/migrate/20171130125155_create_answers.rb
+++ db/migrate/20171130125155_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
       t.string :body
-      t.boolean :correct
+      t.boolean :correct, default: 0
       t.integer :question_id
 
       t.timestamps
--- db/migrate/20171130165143_add_answer_correct_default.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswerCorrectDefault < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default(:answers, :correct, from: nil, to: 0)
-  end
-end
