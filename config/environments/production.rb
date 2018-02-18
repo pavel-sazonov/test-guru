9d6f81fe69fcd077454cd1066d72a006828159f0
pavel-sazonov <wismyl@ya.ru>
2018-02-18 11:43:48 +0300

    change app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,14 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'shrouded-meadow-23547.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'testguruapp.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               "shrouded-meadow-23547.herokuapp.com",
+    domain:               "testguruapp.herokuapp.com",
     authentication:       "plain",
     user_name:            ENV["SMTP_USERNAME"],
     password:             ENV["SMTP_PASSWORD"],
