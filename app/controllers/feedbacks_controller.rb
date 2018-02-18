e14c4ffaa265b4c16f2329a7115439e266973393
pavel-sazonov <wismyl@ya.ru>
2018-02-18 21:44:54 +0300

    add before_action find_admin callback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -9,8 +9,7 @@ class FeedbacksController < ApplicationController
     @feedback = current_user.feedbacks.new(feedback_params)
 
     if @feedback.save
-      admin = User.find_by(type: 'Admin')
-      FeedbacksMailer.sent_feedback(@feedback, admin).deliver_now
+      FeedbacksMailer.sent_feedback(@feedback).deliver_now
       redirect_to new_feedback_path, notice: 'feedback sent'
     else
       render :new, alert: 'feedback did not send'
