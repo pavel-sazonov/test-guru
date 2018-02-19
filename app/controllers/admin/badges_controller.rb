569706861a393e9ca8b55b3c5d6fa38ef485ee08
pavel-sazonov <wismyl@ya.ru>
2018-02-19 16:07:34 +0300

    add badges #show

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,7 +1,15 @@
 class Admin::BadgesController < Admin::BaseController
-  before_action :authenticate_user!
+  before_action :set_badge, only: %i[show]
 
   def index
     @badges = Badge.all
+  end
+
+  def show;end
+
+  private
+
+  def set_badge
+    @badge = Badge.find(params[:id])
   end
 end
