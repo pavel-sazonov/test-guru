9628a0ea26cdfccc2baa17ea7869863e55f621fe
pavel-sazonov <wismyl@ya.ru>
2018-03-17 01:38:16 +0300

    fix variables initialization

--- app/assets/javascripts/utilities/progress_bar.js
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -1,15 +1,14 @@
 document.addEventListener('turbolinks:load', function() {
-  var bar = document.querySelector('.progress-bar')
+  let bar = document.querySelector('.progress-bar')
 
   if (bar) { changeProgress(bar) }
 })
 
 function changeProgress(bar) {
-  questionNumber = bar.dataset.questionNumber
-  questionsCount = bar.dataset.questionsCount
-  percents = ((questionNumber - 1) / questionsCount).toFixed(2) * 100 + '%'
-
-  progress = (questionNumber) ? percents : '100%'
+  let questionNumber = bar.dataset.questionNumber,
+      questionsCount = bar.dataset.questionsCount,
+      percents = ((questionNumber - 1) / questionsCount).toFixed(2) * 100 + '%',
+      progress = (questionNumber) ? percents : '100%'
 
   bar.style.width = bar.textContent = progress
 }
--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -1,20 +1,21 @@
 document.addEventListener('turbolinks:load', function() {
-  var timer = document.getElementById('timer')
+  let timer = document.getElementById('timer')
 
   if (timer) { changeTimer(timer) }
 })
 
 
 function changeTimer(timer) {
-    startPoint = timer.dataset.startPoint
-    timeAmount = timer.dataset.timeAmount
-    timeNow = (Date.now() / 1000).toFixed()
-    passedTime = timeNow - startPoint
+    let startPoint = timer.dataset.startPoint,
+        timeAmount = timer.dataset.timeAmount,
+        timeNow = (Date.now() / 1000).toFixed(),
+        passedTime = timeNow - startPoint
 
     if (passedTime >= timeAmount) {
         location.href = timer.dataset.resultLink
     }
 
-    timer.textContent = timeAmount - (passedTime)
+    timer.textContent = timeAmount - passedTime
+
     setTimeout(changeTimer, 1000, timer);
 }
